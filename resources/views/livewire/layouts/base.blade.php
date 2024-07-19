0bb7b96657648b80e1ecde5190cb858ef2009233
SenaEnana <senaad47@gmail.com>
2024-07-19 22:26:34 +0300

    new package added

--- resources/views/livewire/layouts/base.blade.php
+++ resources/views/livewire/layouts/base.blade.php
@@ -4,7 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Maintenance request management system</title>
+
     {{-- Bootstrap Styles --}}
+
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -19,6 +22,7 @@
 
 {-- Bootstrap Scripts --}
 <script src="{{ mix('js/app.js') }}" defer></script>
+<script src="{{ mix('js/app.js') }}"></script>
 <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" ></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js"></script>
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
 
 Route::get('/employees', App\Livewire\EmployeeComponent::class);
 Route::get('/technicians', App\Livewire\TechnicianComponent::class);
 Route::get('/customers', App\Livewire\Customer::class);
+
+require __DIR__.'/auth.php';
